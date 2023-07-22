9fd79c8694031757e16a73b8cd95cf813d866a3e
Tungetyt <48414404+Tungetyt@users.noreply.github.com>
2023-07-22 20:47:50 +0200

    Always show login selection after login clicked

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,46 @@
-import NextAuth, {AuthOptions} from "next-auth"
+import NextAuth, {AuthOptions, Profile} from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+
+type GoogleProfile = Omit<{
+        iss: string
+        azp: string
+        aud: string
+        sub: `${number}`
+        hd: string
+        email: `${string}@${string}.${string}`
+        email_verified: boolean
+        at_hash: string
+        picture: string
+        given_name: string
+        family_name: string
+        locale: string
+        iat: number
+        exp: number
+} & Profile, "image">
 
 const authOptions = {
     providers: [
         GoogleProvider({
             // TODO: Remove casting
             clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+            authorization: {
+                params: {
+                    prompt: "consent",
+                    access_type: "offline",
+                    response_type: "code"
+                }
+            }
         })
-    ]
+    ],
+    callbacks: {
+        async signIn({ account, profile }) {
+            if (account?.provider === "google")
+                return (profile as GoogleProfile)?.email_verified
+
+            return true // Do different verification for other providers that don't have `email_verified`
+        },
+    }
 } satisfies AuthOptions
 
 const handler = NextAuth(authOptions)
--- app/components/GoogleAuth.tsx
+++ app/components/GoogleAuth.tsx
@@ -1,11 +1,13 @@
 'use client'
 
-import {signIn} from "next-auth/react";
+import {signIn, signOut} from "next-auth/react";
 
 export const GoogleAuth = () => {
     return (
         <>
             <button onClick={() => signIn("google")}>signIn</button>
+            <div></div>
+            <button onClick={() => signOut()}>signOut</button>
         </>
     );
 };
