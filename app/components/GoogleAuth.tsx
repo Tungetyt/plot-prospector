b30143723b1f39462158884a1e6b490502fb92e3
Tungetyt <48414404+Tungetyt@users.noreply.github.com>
2023-07-30 14:40:30 +0200

    Format

--- app/components/GoogleAuth.tsx
+++ app/components/GoogleAuth.tsx
@@ -1,13 +1,13 @@
 'use client'
 
-import {signIn, signOut} from "next-auth/react";
+import { signIn, signOut } from 'next-auth/react'
 
 export const GoogleAuth = () => {
-    return (
-        <>
-            <button onClick={() => signIn("google")}>signIn</button>
-            <div></div>
-            <button onClick={() => signOut()}>signOut</button>
-        </>
-    );
-};
+  return (
+    <>
+      <button onClick={() => signIn('google')}>signIn</button>
+      <div></div>
+      <button onClick={() => signOut()}>signOut</button>
+    </>
+  )
+}
