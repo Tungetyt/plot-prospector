6970646ef2ba88cd42405458625c8e4f5e046bfb
Michal Kotlicki <michal.kotlicki@pwc.com>
2023-08-12 19:01:50 +0200

    Refactor Map Events

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -6,41 +6,52 @@ import 'leaflet-defaulticon-compatibility'
 import { MapContainer, Polygon, TileLayer, useMap } from 'react-leaflet'
 import { useEffect, useRef } from 'react'
 import { useRouter } from 'next/navigation'
+import { MapOptions } from 'leaflet'
 import { useDraftPlot, usePhase } from '@/app/store/draftPlot/draftPlotStore'
 import isNumeric from '@/app/utils/common'
 import { Point } from '@/app/store/draftPlot/common'
+import { Mutable } from '@/app/utils/types'
 
 export type LeafPoint = [number, number]
-
-const purpleOptions = { color: 'blue' }
 
 export const formatPlot = (plot: readonly Point[]) =>
   plot
     .filter(({ lat, lng }) => isNumeric(lat) && isNumeric(lng))
     .map(({ lat, lng }) => [lat, lng] as LeafPoint)
 
-function useMapInitialization(
+type Lng = keyof Pick<Point, 'lng'>
+type Lat = keyof Pick<Point, 'lat'>
+type Zoom = keyof Pick<MapOptions, 'zoom'>
+
+const urlParams = {
+  lat: 'lat',
+  lng: 'lng',
+  zoom: 'zoom',
+} as const satisfies Readonly<
+  Record<Lng, Lng> & Record<Lat, Lat> & Record<Zoom, Zoom>
+>
+
+const useMapInitialization = (
   map: ReturnType<typeof useMap>,
   searchParams: URLSearchParams,
-) {
+) => {
   const mapInitialized = useRef(false)
 
   useEffect(() => {
-    if (mapInitialized.current) {
-      return
-    }
+    if (mapInitialized.current) return
 
-    const lat = searchParams.get('lat')
-    const lng = searchParams.get('lng')
-    const zoom = searchParams.get('zoom')
+    mapInitialized.current = true
 
-    if (lat && lng && zoom) {
-      map.setView(
-        [parseFloat(lat as string), parseFloat(lng as string)],
-        parseInt(zoom as string, 10),
-      )
-      mapInitialized.current = true
-    }
+    const lat = searchParams.get(urlParams.lat)
+    const lng = searchParams.get(urlParams.lng)
+    const zoom = searchParams.get(urlParams.zoom)
+
+    if (!lat || !lng || !zoom) return
+
+    map.setView(
+      [parseFloat(lat as string), parseFloat(lng as string)],
+      parseInt(zoom as string, 10),
+    )
   }, [searchParams, map])
 }
 
@@ -54,7 +65,7 @@ function MapEvents() {
 
   useEffect(() => {
     const updateUrl = () => {
-      const center = map.getCenter()
+      const { lng, lat } = map.getCenter()
       const zoom = map.getZoom()
 
       const queryString = new URLSearchParams({
@@ -62,9 +73,9 @@ function MapEvents() {
           (obj, [key, val]) => ({ ...obj, [key]: val }),
           {},
         ),
-        lat: center.lat.toString(),
-        lng: center.lng.toString(),
-        zoom: zoom.toString(),
+        [urlParams.lat]: lat.toString(),
+        [urlParams.lng]: lng.toString(),
+        [urlParams.zoom]: zoom.toString(),
       }).toString()
 
       const newPath = `${window.location.pathname}?${queryString}`
@@ -82,6 +93,10 @@ function MapEvents() {
   return null
 }
 
+const PWJozefoslawObservatoryCoordinates: Readonly<[number, number]> = [
+  52.0979030011665, 21.03239659105818,
+] as const
+
 function Map() {
   const phase = usePhase()
   const draftPlot = useDraftPlot()
@@ -94,7 +109,11 @@ function Map() {
       }`}
     >
       <MapContainer
-        center={[52.0979030011665, 21.03239659105818]}
+        center={
+          PWJozefoslawObservatoryCoordinates as Mutable<
+            typeof PWJozefoslawObservatoryCoordinates
+          >
+        }
         zoom={13}
         className="flex-1"
         attributionControl={false}
@@ -102,7 +121,7 @@ function Map() {
       >
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         <Polygon
-          pathOptions={purpleOptions}
+          pathOptions={{ color: 'blue' }}
           positions={newPlot}
           eventHandlers={{
             add: ({ target }) => {
