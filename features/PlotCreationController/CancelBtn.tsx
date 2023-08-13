bb82fa9d89775510704a94d5a426a1fc2897b947
Michal Kotlicki <michal.kotlicki@pwc.com>
2023-08-13 13:04:19 +0200

    Cancel Plot Dialog

--- features/PlotCreationController/CancelBtn.tsx
+++ features/PlotCreationController/CancelBtn.tsx
@@ -1,5 +1,9 @@
 import { useTranslations } from 'next-intl'
+import { createPortal } from 'react-dom'
 import { Store } from '@/store/draftPlot/common'
+
+const dialogId = 'cancelPlotModal'
+const body = document.getElementsByTagName('body')[0]
 
 function CancelBtn({
   setState,
@@ -7,14 +11,50 @@ function CancelBtn({
   setState: Store['actions']['changePhase']
 }) {
   const t = useTranslations('Index')
+
+  if (!body) throw new Error('Expected body to be in DOM')
+
   return (
-    <button
-      type="button"
-      className="btn  btn-error text-center"
-      onClick={() => setState('')}
-    >
-      {t('Cancel_Plot')}
-    </button>
+    <>
+      <button
+        type="button"
+        className="btn btn-error text-center mb-2"
+        onClick={() =>
+          (
+            window as Window &
+              typeof globalThis & {
+                [dialogId]: {
+                  showModal: () => void
+                }
+              }
+          )[dialogId].showModal()
+        }
+      >
+        {t('Cancel_Plot')}
+      </button>
+      {createPortal(
+        <dialog id={dialogId} className="modal">
+          <form method="dialog" className="modal-box">
+            <h3 className="font-bold text-lg">{t('Cancel_Plot')}</h3>
+            <p className="pt-4">{t('Cancel_Plot_desc')}</p>
+            <p className="pb-4">{t('Cancel_Plot_desc2')}</p>
+            <div className="modal-action">
+              <button
+                className="btn btn-error"
+                type="submit"
+                onClick={() => setState('')}
+              >
+                {t('Cancel_Plot')}
+              </button>
+            </div>
+          </form>
+          <form method="dialog" className="modal-backdrop">
+            <button type="submit">close</button>
+          </form>
+        </dialog>,
+        body,
+      )}
+    </>
   )
 }
 
