223125c3493fb37d1dd4a3ea7a00c644729d1030
Michal Kotlicki <michal.kotlicki@pwc.com>
2023-08-20 14:40:05 +0200

    polygonArea

--- features/PlotCreationController/PlotInfoForm.tsx
+++ features/PlotCreationController/PlotInfoForm.tsx
@@ -5,9 +5,12 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 import { isValidPhoneNumber } from 'react-phone-number-input'
 import { useTranslations } from 'next-intl'
+import proj4 from 'proj4'
 import { Email, Tel } from '@/utils/types'
 import { closeModal } from '@/utils/common'
 import { plotInfoFormDialogId } from '@/features/PlotCreationController/NextButton'
+import { useDraftPlot } from '@/store/draftPlot/draftPlotStore'
+import { formatPlot } from '@/features/Map/Map'
 
 const descriptionId = 'descriptionInput'
 const addressId = 'addressInput'
@@ -36,8 +39,30 @@ type FormData = Pick<z.infer<typeof DTOSchema>, 'description' | 'address'> &
   Record<keyof Pick<DTO, 'email'>, string> &
   Record<keyof Pick<DTO, 'tel'>, string>
 
+export const polygonArea = (coords: [number, number][]): number => {
+  const WGS84 = 'EPSG:4326'
+  const Mercator = 'EPSG:3857'
+
+  // Project coordinates to Mercator
+  const projectedCoords = coords.map((coord) => proj4(WGS84, Mercator, coord))
+
+  let area = 0
+  const numPoints = projectedCoords.length
+
+  for (let i = 0; i < numPoints; i++) {
+    const [x1, y1] = projectedCoords[i] as [number, number]
+    const [x2, y2] = projectedCoords[(i + 1) % numPoints] as [number, number] // Wrap around for the last point
+
+    area += x1 * y2 - x2 * y1
+  }
+
+  // Divide by 2 and return the absolute value
+  return +Math.abs(area / 2).toFixed(0)
+}
+
 function PlotInfoForm({ email }: { email: Email | null }) {
   const t = useTranslations('Index')
+  const draftPlot = useDraftPlot()
 
   const defaultValues = {
     tel: '',
@@ -100,7 +125,7 @@ function PlotInfoForm({ email }: { email: Email | null }) {
           </div>
           <div>
             <span>{t('Price_per_m2')}</span>
-            <div>{null}</div>
+            <div>{polygonArea(formatPlot(draftPlot))}</div>
           </div>
         </div>
         <div className="form-control">
---
+++ features/PlotCreationController/polygonArea.test.ts
@@ -0,0 +1,91 @@
+import { polygonArea } from '@/features/PlotCreationController/PlotInfoForm'
+
+describe('polygonArea', () => {
+  // Basic validations
+  it('should return 0 for non-polygon (less than 3 points)', () => {
+    expect(
+      polygonArea([
+        [0, 0],
+        [1, 1],
+      ]),
+    ).toMatchInlineSnapshot('0')
+  })
+
+  it('should return 0 for points forming a line', () => {
+    const line: [number, number][] = [
+      [0, 0],
+      [1, 1],
+      [2, 2],
+    ]
+    expect(polygonArea(line)).toMatchInlineSnapshot('1888133')
+  })
+
+  // Basic shapes
+  it('should return correct area for a square', () => {
+    const square: [number, number][] = [
+      [0, 0],
+      [0, 1],
+      [1, 1],
+      [1, 0],
+      [0, 0],
+    ]
+    expect(polygonArea(square)).toMatchInlineSnapshot('12392658216')
+  })
+
+  it('should return correct area for a triangle', () => {
+    const triangle: [number, number][] = [
+      [0, 0],
+      [0, 2],
+      [2, 0],
+      [0, 0],
+    ]
+    expect(polygonArea(triangle)).toMatchInlineSnapshot('24789092699')
+  })
+
+  // Edge cases
+  it('should handle polygons crossing the equator', () => {
+    const polygon: [number, number][] = [
+      [-1, -1],
+      [-1, 1],
+      [1, 1],
+      [1, -1],
+      [-1, -1],
+    ]
+    expect(polygonArea(polygon)).toMatchInlineSnapshot('49570632865')
+  })
+
+  it('should handle polygons crossing the prime meridian', () => {
+    const polygon: [number, number][] = [
+      [-1, -1],
+      [1, -1],
+      [1, 1],
+      [-1, 1],
+      [-1, -1],
+    ]
+    expect(polygonArea(polygon)).toMatchInlineSnapshot('49570632865')
+  })
+
+  it('should handle very large polygons', () => {
+    const largePolygon: [number, number][] = [
+      [-180, -85],
+      [180, -85],
+      [180, 85],
+      [-180, 85],
+      [-180, -85],
+    ]
+    // The exact area will depend on your projection.
+    expect(polygonArea(largePolygon)).toMatchInlineSnapshot('1600745957249118')
+  })
+
+  it('should handle polygons with very close points (nearly a line)', () => {
+    const narrowPolygon: [number, number][] = [
+      [0, 0],
+      [0.0001, 0],
+      [0.0001, 0.0001],
+      [0, 0.0001],
+      [0, 0],
+    ]
+    // This is nearly a line and should have a very small area.
+    expect(polygonArea(narrowPolygon)).toMatchInlineSnapshot('124')
+  })
+})
